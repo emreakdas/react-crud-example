d2bbc8544545468c5e594107c09dff310cf3706c
Emre AKDAŞ <eemreakdas@gmail.com>
2023-11-16 01:57:24 +0300

    overflow change

--- src/components/Table/Index.jsx
+++ src/components/Table/Index.jsx
@@ -5,7 +5,7 @@ import Cell from "./Cell";
 
 function Table({ children, thead }) {
   return (
-    <div className="overflow-x-auto">
+    <div className="overflow-x-auto overflow-y-visible">
       <table className="min-w-full divide-y divide-gray-700">
         <Thead theads={thead} />
         <Tbody>{children}</Tbody>
