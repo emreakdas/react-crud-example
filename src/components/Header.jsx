ec47139ca92e48d9f5645daa38d862c26c777120
Emre AKDAŞ <eemreakdas@gmail.com>
2023-12-02 17:47:54 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-gray-800 shadow">
       <div className="container mx-auto p-3 flex justify-between items-center">
-        <Link to="/" className="text-2xl uppercase font-mono text-white">Verd<small>export</small></Link>
+        <Link to="/" className="text-2xl uppercase font-mono text-white">Crud Example</Link>
         <Link to="/users" className="text-md text-white">Users</Link>
       </div>
     </header>
